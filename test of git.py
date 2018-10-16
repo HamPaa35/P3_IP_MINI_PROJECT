2299044a380bfe2ec15d5b435644d423b631716e
Mathias <mfaldt17@student.aau.dk>
2018-10-16 16:44:02 +0200

    Does not work, some math stuff is now added

--- test of git.py
+++ test of git.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import cv2
 
@@ -14,10 +16,36 @@ def calculateH():
     for i in range(len(imageData)):
         for j in range(len(imageData[0])):
             pixel = imageData[i][j]
-            R = pixel[0]
+            B = pixel[0]
             G = pixel[1]
-            B = pixel[2]
-            hValue= 200
+            R = pixel[2]
+            test1 = (R-G)+(R-B)
+            if test1 > 0:
+                test1 = test1 / 2
+            else:
+                test1 = 1
+                test1 = test1 / 2
+            if test1 > 0 and math.sqrt(math.pow(R - G, 2) + (R - B) * (G - B)) > 0:
+                test1 = test1 / math.sqrt(math.pow(R - G, 2) + (R - B) * (G - B))
+            else:
+                test1 = 1
+                test1 = test1 / (math.sqrt(math.pow(R - G, 2) + (R - B) * (G - B))+1)
+            if test1 > 0:
+                test1 = test1/360
+            else:
+                test1 = 1
+                test1 = test1 / 360
+
+            hAngle = math.degrees(math.acos(test1))
+
+
+            hValue= 0
+            if B <= G:
+                hValue = hAngle
+            elif G<B:
+                hValue = 360-hAngle
+            else:
+                hValue = 0
             outputForHValue[i, j, 0] = hValue
             outputForHValue[i, j, 1] = hValue
             outputForHValue[i, j, 2] = hValue
@@ -27,9 +55,9 @@ def calculateS():
     for i in range(len(imageData)):
         for j in range(len(imageData[0])):
             pixel = imageData[i][j]
-            R = pixel[0]
+            B = pixel[0]
             G = pixel[1]
-            B = pixel[2]
+            R = pixel[2]
             sValue = 255-(3*min(R, G, B))
             outputForSValue[i, j, 0] = sValue
             outputForSValue[i, j, 1] = sValue
@@ -40,9 +68,9 @@ def calculateI():
     for i in range(len(imageData)):
         for j in range(len(imageData[0])):
             pixel = imageData[i][j]
-            R = pixel[0]
+            B = pixel[0]
             G = pixel[1]
-            B = pixel[2]
+            R = pixel[2]
             iValue = (R+G+B)/3
             outputForIValue[i, j, 0] = iValue
             outputForIValue[i, j, 1] = iValue
