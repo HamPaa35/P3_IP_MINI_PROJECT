5e3074c591191e8499d3bc4a53ce9d8dddcf3baa
Mathias <mfaldt17@student.aau.dk>
2018-10-20 17:01:38 +0200

    Comments for the code has been added.
    Added second method of code, for calculating the HSI values, still very blocky and noisy

--- RGB2HSI_Book.py
+++ RGB2HSI_Book.py
@@ -1,202 +1,101 @@
 import math
-
 import numpy as np
 import cv2
 
-image = cv2.imread("flowers.jpg")
-height, width,  channels = image.shape
+# This code converts RGB to HSI using the method and math discussed in the IP book:
+# Introduction to Video and Image Processing by Thomas B. Moeslund, Appendix D: Conversion Between RGB and HSI
 
+# Here the code imports the image that the algorithm will use.
+image = cv2.imread("flowers.jpg")
+# The dimensions of the image is stored in different variables
+height, width, channels = image.shape
+
+# Three empty arrays are made, for the output images
 outputForHValueBook = np.zeros((height, width, channels), np.uint64)
 outputForSValueBook = np.zeros((height, width, channels), np.uint64)
 outputForIValueBook = np.zeros((height, width, channels), np.uint64)
 outputForHSIValueBook = np.zeros((height, width, channels), np.uint64)
 
+# The data from the imported image is stored in a numpy array
 imageData = np.asarray(image)
 
+
+# This function calculates the Hue in the HSI color space
 def calculateHBook(R, G, B):
+    # Here the code will try to perform the calculation of the theta, but if an error happens, like a division by zero
+    # the code will set the theta to 0 and move on.
     try:
-        hAngle = math.degrees(math.acos(0.5 * (((R - G) + (R - B)) / math.sqrt((R-G)*(R-G) + (R - B) * (G - B)))))
+        # The math(from the book) for calculating the theta is implemented in this line of code.
+        hAngle = math.degrees(math.acos(0.5 * (((R - G) + (R - B)) / math.sqrt((R - G) * (R - G) + (R - B) * (G - B)))))
     except:
         hAngle = 0
-    # if hAngle < 0:
-    #     hAngle *= -1
 
+    # Here the code checks the value of the Blue and the green channel, this will have an effect on the final Hue value
     hValue = 0
     if B <= G:
         hValue = hAngle
     else:
         hValue = 360 - hAngle
-    print(hAngle, hValue)
     return hValue
-    #thetaBeforeCos = (0.5 * (((R - G) + (R - B)) / math.sqrt((R-G)*(R-G) + (R - B) * (G - B))))
-
-    #hAngle = 0
-
-    # if thetaBeforeCos > 1:
-    #     hAngle = math.degrees(math.acos(thetaBeforeCos))
-    #
-    # elif thetaBeforeCos < -1:
-    #     hAngle = math.degrees(math.acos(thetaBeforeCos))
-    #
-    # else:
-    #     hAngle = math.degrees(math.acos(thetaBeforeCos))
-
-    #if R<0 and G<0 and B<0 or R>1 and G>1 and B>1:
-    #    return 0
-    #else:
-
-    # if s == 0:
-    #     return 0
-    #
-    # elif rgbMax == R:
-    #     return (G-B)/(rgbMax-rgbMin)
-    #
-    # elif rgbMax == G:
-    #     return 2.0 + (B-R)/(rgbMax-rgbMin)
-    #
-    # elif rgbMax == B:
-    #     return 4.0 + (R-G)/(rgbMax-rgbMin)
-    #
-    # else:
-    #     return 0
 
 
+# This is the main function in the programme, this converts the RGB values to HSI
 def calculateHSIBook():
+    # This nested for-loop goes through the columns and rows of the imported image
+    # The i value goes through the rows and the j value each of the columns in the i rows
     for i in range(len(imageData)):
         for j in range(len(imageData[0])):
-            pixel = imageData[i][j]
-            B = float(pixel[0]/255)
-            G = float(pixel[1]/255)
-            R = float(pixel[2]/255)
 
-            iValue = (R+G+B)/3.0
+            # The color values are imported for each pixel in the loop
+            pixel = imageData[i][j]
+            B = float(pixel[0] / 255)
+            G = float(pixel[1] / 255)
+            R = float(pixel[2] / 255)
+
+            # Here the Intensity is calculated, and put into the output image, in grey scale.
+            iValue = (R + G + B) / 3.0
             outputForIValueBook[i, j, 0] = iValue * 255
             outputForIValueBook[i, j, 1] = iValue * 255
             outputForIValueBook[i, j, 2] = iValue * 255
 
-            try:
-                sValue = 1.0-(3.0*((min(R, G, B))/(R+G+B)))
+            # Here the Saturation is calculated, and the value is them again put into the output image, in grey scale
+            try:  # Another try, in case of division by zero errors
+                sValue = 1.0 - (3.0 * ((min(R, G, B)) / (R + G + B)))
             except:
                 sValue = 0
-            print(sValue)
+
             outputForSValueBook[i, j, 0] = sValue * 255
             outputForSValueBook[i, j, 1] = sValue * 255
             outputForSValueBook[i, j, 2] = sValue * 255
 
-            try:
+            # Here the Hue is calculated, simply by calling the previously declared function, with the current pixels
+            # RGB values. The calculated values are once again put into the output image in grey scale.
+            try:  # Another try, in case of division by zero errors
                 hValue = calculateHBook(R, G, B)
             except:
-                hValue=0
-            # if hValue < 0:
-            #     hValue = (hValue*60)+360
-            # else:
-            #     hValue = hValue*60
-            #if np.isnan(hValue):
-            #    hValue = 0
+                hValue = 0
             outputForHValueBook[i, j, 0] = (hValue / 360) * 255
             outputForHValueBook[i, j, 1] = (hValue / 360) * 255
             outputForHValueBook[i, j, 2] = (hValue / 360) * 255
 
-            #print(hValue, sValue, iValue)
-
+            # Here the HSI values are put into the same output image, this has no real purpose,
+            # and is mostly just interesting to look at :)
             outputForHSIValueBook[i, j, 0] = (hValue / 360) * 255
             outputForHSIValueBook[i, j, 1] = sValue * 255
             outputForHSIValueBook[i, j, 2] = iValue * 255
 
 
+# The main function is called
 calculateHSIBook()
 
+# The the previously blank images are saved to the hard drive and can now be view outside this programme
 cv2.imwrite('outputHValueBook.jpg', outputForHValueBook)
 cv2.imwrite('outputSValueBook.jpg', outputForSValueBook)
 cv2.imwrite('outputIValueBook.jpg', outputForIValueBook)
 cv2.imwrite('outputHSIValueBook.jpg', outputForHSIValueBook)
-print('The code completed book')
 
-#Try to implement this math http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
-# Some problems may be the fact that it does not work with pure balck and white
+# Simple debug message
+print('The code from the book completed')
 
-
-# def calculateS(i, rgbMin, rgbMax):
-#     if rgbMin == rgbMax:
-#         return 0
-#
-#     elif i < 0.5:
-#         return (rgbMax-rgbMin)/(rgbMax+rgbMin)
-#
-#     elif i > 0.5:
-#         return (rgbMax-rgbMin)/(2.0-rgbMax-rgbMin)
-#
-#     else:
-#         return 0
-
-# def calculateH():
-#     for i in range(len(imageData)):
-#         for j in range(len(imageData[0])):
-#             pixel = imageData[i][j]
-#             B = pixel[0]
-#             G = pixel[1]
-#             R = pixel[2]
-#              test1 = (R-G)+(R-B)
-#              if test1 > 0:
-#                  test1 = test1 / 2
-#              else:
-#                  test1 = 1
-#                  test1 = test1 / 2
-#              if test1 > 0 and math.sqrt(math.pow(R - G, 2) + (R - B) * (G - B)) > 0:
-#                  test1 = test1 / math.sqrt(math.pow(R - G, 2) + (R - B) * (G - B))
-#              else:
-#                  test1 = 1
-#                  test1 = test1 / (math.sqrt(math.pow(R - G, 2) + (R - B) * (G - B))+1)
-#              if test1 > 0:
-#                  test1 = test1/360
-#              else:
-#                  test1 = 1
-#                  test1 = test1 / 360
-#
-#              hAngle = math.degrees(math.acos(test1))
-#
-#
-#              hValue= 0
-#              if B <= G:
-#                  hValue = hAngle
-#              elif G<B:
-#                  hValue = 360-hAngle
-#              else:
-#                  hValue = 0
-#             outputForHValue[i, j, 0] = hValue
-#             outputForHValue[i, j, 1] = hValue
-#             outputForHValue[i, j, 2] = hValue
-#
-#
-# def calculateS():
-#     for i in range(len(imageData)):
-#         for j in range(len(imageData[0])):
-#             pixel = imageData[i][j]
-#             B = pixel[0]
-#             G = pixel[1]
-#             R = pixel[2]
-#             sValue = 1-(3*min(R, G, B))
-#             outputForSValue[i, j, 0] = sValue
-#             outputForSValue[i, j, 1] = sValue
-#             outputForSValue[i, j, 2] = sValue
-#
-#
-# def calculateI():
-#     for i in range(len(imageData)):
-#         for j in range(len(imageData[0])):
-#             pixel = imageData[i][j]
-#             B = pixel[0]
-#             G = pixel[1]
-#             R = pixel[2]
-#             iValue = (R+G+B)/3
-#             outputForIValue[i, j, 0] = iValue
-#             outputForIValue[i, j, 1] = iValue
-#             outputForIValue[i, j, 2] = iValue
-#
-#
-# calculateH()
-# calculateS()
-# calculateI()
-#math.acos(((R - G) + (R - B) / 2) / (math.sqrt(math.pow(R - G, 2) + ((R - B) * (G - B)))))
-
-
+# Try to implement this math http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
+# Some problems may be the fact that it does not work with pure black and white
--- RGB3HSI_Web.py
+++ RGB3HSI_Web.py
@@ -1,25 +1,37 @@
-import math
-
 import numpy as np
 import cv2
 
+# This code converts RGB to HSI using the method and math discussed on the web page:
+# https://www.codeproject.com/Articles/8174/Class-to-calculate-HSI-Hue-Saturation-Intensity Found 17/10 - 2018
+
+# This code is included because it is more efficient than the method from the book, but not af accurate.
+
+# Here the code imports the image that the algorithm will use.
 image = cv2.imread("flowers.jpg")
+# The dimensions of the image is stored in different variables
 height, width,  channels = image.shape
 
+# Three empty arrays are made, for the output images
 outputForHValueWeb = np.zeros((height, width, channels), np.uint64)
 outputForSValueWeb = np.zeros((height, width, channels), np.uint64)
 outputForIValueWeb = np.zeros((height, width, channels), np.uint64)
 outputForHSIValueWeb = np.zeros((height, width, channels), np.uint64)
 
+# The data from the imported image is stored in a numpy array
 imageData = np.asarray(image)
 
+
+# This function calculates the Hue in the HSI color space
 def calculateHWeb(R, G, B, max, dif, sValue):
 
     hValue = 0
 
+    # Here the different values of the Hue is calculated based on what color has the highest value in the current pixel.
+    # Compared to the method from the book this is more efficient,
+    # because it does not require the use of trigonometric functions. It can however be less accurate
     if sValue is not 0:
         if max == R:
-            if dif == 0:
+            if dif == 0:  # included to avoid division by zero
                 dif = 1
             hValue = (60*(G-B)/dif)
         elif max == G:
@@ -33,6 +45,8 @@ def calculateHWeb(R, G, B, max, dif, sValue):
 
     return hValue
 
+
+# This function calculates the Saturation in the HSI color space
 def calculateSWeb(iValue, sum, dif):
     if sum == 0:
         sum = 1
@@ -42,19 +56,25 @@ def calculateSWeb(iValue, sum, dif):
         return 255*(dif/(510-sum))
 
 
+# This function calculates the Intensity in the HSI color space
 def calculateIWeb(sum):
     return sum/2
 
 
-
+# This is the main function in the programme, this converts the RGB values to HSI
 def calculateHSIWeb():
+    # This nested for-loop goes through the columns and rows of the imported image
+    # The i value goes through the rows and the j value each of the columns in the i rows
     for i in range(len(imageData)):
         for j in range(len(imageData[0])):
+
+            # The color values are imported for each pixel in the loop
             pixel = imageData[i][j]
             B = pixel[0]
             G = pixel[1]
             R = pixel[2]
 
+            # The maximum, minimum, sum and difference values are calculated, from the RGB values at the current pixel
             nImax = max(R, B)
             nImax = max(nImax, G)
             nImin = min(R, B)
@@ -62,8 +82,9 @@ def calculateHSIWeb():
             nSum = nImin + nImax
             nDifference = nImax - nImin
 
-            if R<0 and G<0 and B<0 or R>255 and G>255 and B >255:
-                hValue=sValue=iValue=0
+            # The HSI values are simply set to 0 if the RGB values are not within 0 and 255
+            if R < 0 and G < 0 and B < 0 or R > 255 and G > 255 and B > 255:
+                hValue = sValue = iValue = 0
 
                 outputForIValueWeb[i, j, 0] = iValue
                 outputForIValueWeb[i, j, 1] = iValue
@@ -81,16 +102,26 @@ def calculateHSIWeb():
                 outputForHSIValueWeb[i, j, 1] = sValue
                 outputForHSIValueWeb[i, j, 2] = iValue
             else:
+
+                # Here the Intensity is calculated, simply by calling the previously declared function,
+                # with the current pixels RGB values.
+                # The calculated values are then put into the output image in grey scale.
                 iValue = calculateIWeb(nSum)
                 outputForIValueWeb[i, j, 0] = iValue
                 outputForIValueWeb[i, j, 1] = iValue
                 outputForIValueWeb[i, j, 2] = iValue
 
+                # Here the Saturation is calculated, simply by calling the previously declared function,
+                # with the current pixels RGB values.
+                # The calculated values are then once again put into the output image in grey scale.
                 sValue = calculateSWeb(iValue, nSum, nDifference)
                 outputForSValueWeb[i, j, 0] = sValue
                 outputForSValueWeb[i, j, 1] = sValue
                 outputForSValueWeb[i, j, 2] = sValue
 
+                # Here the Hue is calculated, simply by calling the previously declared function,
+                # with the current pixels RGB values.
+                # The calculated values are once again put into the output image in grey scale.
                 hValue = calculateHWeb(R, G, B, nImax, nDifference, sValue)
                 if hValue>255:
                     hValue=255
@@ -99,19 +130,22 @@ def calculateHSIWeb():
                 outputForHValueWeb[i, j, 0] = hValue
                 outputForHValueWeb[i, j, 1] = hValue
                 outputForHValueWeb[i, j, 2] = hValue
-                print(hValue, sValue, iValue)
+
+                # Here the HSI values are put into the same output image, this has no real purpose,
+                # and is mostly just interesting to look at :)
                 outputForHSIValueWeb[i, j, 0] = hValue
                 outputForHSIValueWeb[i, j, 1] = sValue
                 outputForHSIValueWeb[i, j, 2] = iValue
 
 
+# The main function is called
 calculateHSIWeb()
 
+# The the previously blank images are saved to the hard drive and can now be view outside this programme
 cv2.imwrite('outputHValueWeb.jpg', outputForHValueWeb)
 cv2.imwrite('outputSValueWeb.jpg', outputForSValueWeb)
 cv2.imwrite('outputIValueWeb.jpg', outputForIValueWeb)
 cv2.imwrite('outputHSIValueWeb.jpg', outputForHSIValueWeb)
-print('The code completed Web')
 
-#Try to implement this math http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
-# Some problems may be the fact that it does not work with pure balck and white
+# Simple debug message
+print('The code from the web completed')
